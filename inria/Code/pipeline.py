841f20e1e7c03785f7bf59d164e022f4f3b21e52
maya-T <mayatouzari@gmail.com>
2021-07-26 12:02:56 +0200

    fixed bug

--- inria/Code/pipeline.py
+++ inria/Code/pipeline.py
@@ -25,65 +25,66 @@ with requests.get("https://geolocation-db.com/json") as url:
     ip_address = data["IPv4"]
 
 with open("queries.txt", "a") as a_file:
-    # while True:
-    # query = input("\n\nPlease enter a new query: ")
-    query = "Quel est le niveau de la nappe à Orléans du 15 mars 2019 au 30 mars 2019 "
-    a_file.write(query)
-    a_file.write("\n")
-    ####################################################################################################################
+    while True:
+        query = input("\n\nPlease enter a new query: ")
+        #
+        query = "Quel est le niveau de la nappe à Orléans du 15 mars 2019 au 30 mars 2019 "
+        a_file.write(query)
+        a_file.write("\n")
+        ####################################################################################################################
 
-    dates_to_keep = get_time(query)
-    print(colored("Extracted temporal expressions:\n", "green"))
-    pprint(dates_to_keep)
-    print("\n-----------------------------------------------------------------------------------\n")
+        dates_to_keep = get_time(query)
+        print(colored("Extracted temporal expressions:\n", "green"))
+        pprint(dates_to_keep)
+        print("\n-----------------------------------------------------------------------------------\n")
 
-    ####################################################################################################################
-    data = []
-    bss_codes = get_bss(query)
-    if len(bss_codes) > 0:
-        print(colored("Extracted bss codes: \n", "green"))
-        print(bss_codes)
-        if len(dates_to_keep) > 0:
-            for date in dates_to_keep:
-                data.append(get_table_data_bss(bss_codes, start_date=date.get("start_date", None),
-                                               end_date=date.get("end_date", None)))
+        ####################################################################################################################
+        data = []
+        bss_codes = get_bss(query)
+        if len(bss_codes) > 0:
+            print(colored("Extracted bss codes: \n", "green"))
+            print(bss_codes)
+            if len(dates_to_keep) > 0:
+                for date in dates_to_keep:
+                    data.append(get_table_data_bss(bss_codes, start_date=date.get("start_date", None),
+                                                   end_date=date.get("end_date", None)))
+            else:
+                data.append(get_table_data_bss(bss_codes))
+
         else:
-            data.append(get_table_data_bss(bss_codes))
 
-    else:
+            locations, method, exact_match, similar = get_locations(query, all_locations, demonym_dict, MODEL_PATH,
+                                                                    ip_address=ip_address)
+            communes, departements, regions = classify(query, locations, method, exact_match, similar)
 
-        locations, method, exact_match, similar = get_locations(query, all_locations, demonym_dict, MODEL_PATH,
-                                                                ip_address=ip_address)
-        communes, departements, regions = classify(query, locations, method, exact_match, similar)
+            print(colored("After classifying the locations:", "red"))
+            print("Communes\n")
+            pprint(communes)
+            print("\nDepartements\n")
+            pprint(departements)
+            print("\nRegions\n")
+            pprint(regions)
 
-        print(colored("After classifying the locations:", "red"))
-        print("Communes\n")
-        pprint(communes)
-        print("\nDepartements\n")
-        pprint(departements)
-        print("\nRegions\n")
-        pprint(regions)
+            deps_to_keep, coms_to_keep, regs_to_keep = get_relevant(communes, departements, regions)
 
-        deps_to_keep, coms_to_keep, regs_to_keep = get_relevant(communes, departements, regions)
+            print(colored("Relevant Locations that will be used to get stations:", "red"))
+            print("Communes:\n")
+            pprint(coms_to_keep)
+            print("Departements:\n")
+            pprint(deps_to_keep)
+            print("Regions:\n")
+            pprint(regs_to_keep)
+            if len(dates_to_keep) > 0:
+                for date in dates_to_keep:
+                    data.append(get_table_data_locations(coms_to_keep, deps_to_keep, regs_to_keep,
+                                                         start_date=date["start_date"],
+                                                         end_date=date["end_date"]))
+            else:
+                data.append(get_table_data_locations(coms_to_keep, deps_to_keep, regs_to_keep))
 
-        print(colored("Relevant Locations that will be used to get stations:", "red"))
-        print("Communes:\n")
-        pprint(coms_to_keep)
-        print("Departements:\n")
-        pprint(deps_to_keep)
-        print("Regions:\n")
-        pprint(regs_to_keep)
-        if len(dates_to_keep) > 0:
-            for date in dates_to_keep:
-                data.append(get_table_data_locations(coms_to_keep, deps_to_keep, regs_to_keep,
-                                                     start_date=date["start_date"],
-                                                     end_date=date["end_date"]))
-        else:
-            data.append(get_table_data_locations(coms_to_keep, deps_to_keep, regs_to_keep))
+        print("\n-----------------------------------------------------------------------------------\n")
 
-    print("\n-----------------------------------------------------------------------------------\n")
-
-    for data_ in data:
-        print(data_)
-        table_general = format_table_general(data_, nb_mesures)
-        print(table_general)
+        for data_ in data:
+            print(data_)
+            table_general = format_table_general(data_, nb_mesures)
+            print(table_general)
